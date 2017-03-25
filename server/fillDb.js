3608c50c041da652c1c4c2c57e3edc3828af0ffe
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-03-25 20:19:50 +0100

    removed commented code

--- server/fillDb.js
+++ server/fillDb.js
@@ -39,15 +39,6 @@ db.serialize(function() {
         console.log(rows);
     });
 
-    // var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    // for (var i = 0; i < 10; i++) {
-    //     stmt.run("Ipsum " + i);
-    // }
-    // stmt.finalize();
-
-    // db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-    //     console.log(row.id + ": " + row.info);
-    // });
 });
 
 db.close();
