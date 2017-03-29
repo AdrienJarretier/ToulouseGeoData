295a21c07107399491dd2d349b73a5980a3eaa9d
lauriehouseaux <lma.houseaux@gmail.com>
2017-03-29 15:01:06 +0200

    get on /patinoires send select result from sqlite

--- server/app.js
+++ server/app.js
@@ -11,19 +11,21 @@ app.get('/', function(req, res) {
     res.sendfile('index.html');
 });
 
+app.get('/patinoires', function(req, res) {
+
+    var db = new sqlite3.Database('firstTestDb.db');
+
+    db.serialize(function() {
+
+        db.all("SELECT * FROM patinoires", function(err, rows) {
+            console.log(rows);
+        });
+
+    });
+
+    db.close();
+});
+
 app.listen(config.port, function() {
     console.log('listening on *:' + config.port);
 });
-
-
-var db = new sqlite3.Database('firstTestDb.db');
-
-db.serialize(function() {
-
-    db.all("SELECT * FROM patinoires", function(err, rows) {
-        console.log(rows);
-    });
-
-});
-
-db.close();
