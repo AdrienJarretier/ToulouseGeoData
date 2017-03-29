a4c5af5977c73ca1f6823ae4b20761929036129c
lauriehouseaux <lma.houseaux@gmail.com>
2017-03-29 14:42:33 +0200

    send html file from webServer

--- server/app.js
+++ server/app.js
@@ -1,4 +1,20 @@
 const sqlite3 = require('sqlite3').verbose();
+const express = require('express');
+var app = express();
+
+const config = { port: 9226 };
+
+app.use(express.static(__dirname + '/../client'));
+
+
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
+});
+
+app.listen(config.port, function() {
+    console.log('listening on *:' + config.port);
+});
+
 
 var db = new sqlite3.Database('firstTestDb.db');
 
